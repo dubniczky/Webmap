713fddda0ee7d5d2e111b3de2075866a0565e788
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-06-11 17:59:29 +0200

    Added config

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
 from . import crawler
 from . import extract
+
+from .config import config
+
 from .url import Url
---
+++ config.py
@@ -0,0 +1,6 @@
+import yaml
+
+config: None|dict = None
+
+with open("config.yml") as f:
+    config = yaml.safe_load( f.read() )
