6f2375b7d773ddd14e1defae48f1bc26c0a75ae2
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-06-11 18:42:23 +0200

    Moved ignore list into config, fixed remove method

--- config.py
+++ config.py
@@ -3,7 +3,3 @@ import yaml
 config: None|dict = None
 with open("config.yml") as f:
     config = yaml.safe_load( f.read() )
-
-ignorelist: None|list(str) = None
-with open(config.ignorelist) as f:
-    ignorelist = f.readlines()
--- crawler.py
+++ crawler.py
@@ -3,6 +3,7 @@ import requests
 import extract
 from url import Url
 
+from config import config
 
 def get(url : str) -> str:
     req = requests.get(url, allow_redirects=True)
@@ -20,7 +21,18 @@ def parse_site(url : str) -> list[Url]:
     urls += Url.from_list(host, extract.raw_urls(page))
     urls += Url.from_list(host, extract.hrefs(page))
     urls += Url.from_list(host, extract.srcs(page))
+
+    return remove_ignored(urls)
+
+def remove_ignored(urls : list[Url]) -> list[Url]:
+    for i in reversed(range(0, len(urls))):
+        if str(urls[i]) in config['ignorelist']:
+            del urls[i]
     return urls
+
+def crawl(url : str):
+    searched = [url]
+    urls = []
 
 if __name__ == '__main__':
     print( [str(i) for i in parse_site('http://netpeak.hu')] )
