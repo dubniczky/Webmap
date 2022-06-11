fc8bad097dc3f4982e8ad2ddd6c1796d2961a9f8
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-06-11 18:03:14 +0200

    Renamed media to extensions

--- url.py
+++ url.py
@@ -1,5 +1,10 @@
 from urllib.parse import urlsplit, ParseResult
 
+from config import config
+
+def get_extensions(name):
+    return tuple(config.extensions[name])
+
 class Url:
     pr: ParseResult
     type: str
@@ -12,13 +17,13 @@ class Url:
             self.pr = self.pr._replace(scheme=scheme)
 
         path = self.pr.path.lower()
-        if path.endswith('.html'):
+        if path.endswith(get_extensions('page')):
             self.type = 'page'
-        elif path.endswith(('.js', '.mjs')):
+        elif path.endswith(get_extensions('script')):
             self.type = 'script'
-        elif path.endswith('.css'):
+        elif path.endswith(get_extensions('style')):
             self.type = 'style'
-        elif path.endswith(('.png', '.jpg', '.svg')):
+        elif path.endswith(get_extensions('media')):
             self.type = 'media'
 
     def __str__(self) -> str:
