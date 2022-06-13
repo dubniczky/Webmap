d1061992e1dd9868b66051706f7903f2eb225f51
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-06-13 19:16:17 +0200

    Fixed errors with binary files, and already searched sites

--- webmap/crawler.py
+++ webmap/crawler.py
@@ -6,17 +6,23 @@ from url import Url
 
 from config import config
 
-def get(url : str) -> str:
-    req = requests.get(url, allow_redirects=True)
-    if req:
-        return str(req.content)
-    else:
+def get(url : str) -> str | None:
+    try:
+        req = requests.get(url, allow_redirects=True)
+        if req:
+            return req.content.decode('utf-8')
+        else:
+            return None
+    except:
         return None
 
 def parse_site(url : str) -> list[Url]:
     u = Url(url)
     host = u.host()
     page = get(url)
+
+    if page is None:
+        return []
 
     urls = []
     urls += Url.from_list(host, extract.raw_urls(page))
@@ -31,15 +37,25 @@ def remove_ignored(urls : list[Url]) -> list[Url]:
             del urls[i]
     return urls
 
-def crawl(url : str, searched = []) -> list[Url]:
-    searched += [url]
-    current = parse_site(url)
-    print('Mapping:', url)
-    targets = []
+def crawl(start: Url, url: Url = None, searched = []) -> list[Url]:
+    if url is None:
+        url = start
+    url_s = str(url)
+    print('Mapping:', url_s)
+    searched += [url_s]
+    current = parse_site(url_s)
     for u in current:
-        if u.type in ['page', 'unknown']:
-            targets.append(u)
-            current += crawl(str(u), searched)
+        # Not a page
+        if u.type not in ['page', 'unknown']:
+            continue
+        # Already searched
+        if str(u) in searched:
+            continue
+        # Different website
+        if start.pr.netloc != url.pr.netloc:
+            continue
+
+        current += crawl(start, u, searched)
     return current
 
 
@@ -50,5 +66,5 @@ if __name__ == '__main__':
 
     print('Url:', url)
 
-    urls = [url] + [str(i) for i in crawl(url)]
+    urls = [url] + [str(i) for i in crawl(Url(url))]
     print( urls )
