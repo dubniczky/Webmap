3af8466a7cdf7c7575761707d6ca943d160cb1e9
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-06-15 12:43:26 +0200

    Added mapping statistics at the end of the run

--- webmap/crawler.py
+++ webmap/crawler.py
@@ -1,3 +1,4 @@
+from time import perf_counter
 import requests
 import sys
 
@@ -37,12 +38,13 @@ def remove_ignored(urls : list[Url]) -> list[Url]:
             del urls[i]
     return urls
 
-def crawl(start: Url, url: Url = None, searched = []) -> list[str]:
+def crawl(start: Url, url: Url = None, searched = []) -> tuple[list[str], int]:
     if url is None:
         url = start
     url_s = str(url)
     print('Mapping:', url_s)
     searched += [url_s]
+    map_count = 1
     current = parse_site(url_s)
     for u in current:
         # Not a page
@@ -55,11 +57,16 @@ def crawl(start: Url, url: Url = None, searched = []) -> list[str]:
         if start.pr.netloc != u.pr.netloc:
             continue
 
-        current += crawl(start, u, searched)
-    return current
+        urls, maps = crawl(start, u, searched)
+        current += urls
+        map_count += maps
+    return (current, map_count)
 
 def unique_list(l):
     return list(set(l))
+
+def trimf(f: float) -> str:
+    return str( round(f, 2) )
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -67,9 +74,18 @@ if __name__ == '__main__':
     url = sys.argv[1]
     file = sys.argv[2]
 
+    start_time = perf_counter()
     print('Url:', url)
 
-    urls = unique_list( [url] + [str(i) for i in crawl(Url(url))] )
+    urls, map_count = crawl(Url(url))
+    unique_urls = unique_list( [url] + [str(i) for i in urls] )
+
+    delta_time = trimf( perf_counter() - start_time )
+    print('========')
+    print(f'Mapping completed in: {delta_time}s')
+    print(f'Mapped sites: {map_count}')
+    print(f'Detected endpoints: {len(unique_urls)}')
+
 
     with open(file, 'w') as f:
-        f.write( '\n'.join(urls) )
+        f.write( '\n'.join(unique_urls) )
