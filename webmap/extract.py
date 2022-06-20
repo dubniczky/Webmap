33dfdbd3ab36b826e00f83b625568503b3d47126
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-06-20 13:45:07 +0200

    Added css extractors

--- webmap/extract.py
+++ webmap/extract.py
@@ -1,10 +1,22 @@
 import re
 
-def hrefs(html : str) -> list[str]:
-    return re.findall(r'href="(.*?)"', html) or []
-
-def srcs(html : str) -> list[str]:
-    return re.findall(r'src="(.*?)"', html) or []
+## General Extractors
 
 def raw_urls(html : str) -> list[str]:
     return re.findall(r'(https?://.*?)[^A-Za-z0-9-_./?#:@!$&%*+,;=~]', html) or []
+
+## HTML Extractors
+
+def html_hrefs(html : str) -> list[str]:
+    return re.findall(r'href="(.*?)"', html) or []
+
+def html_srcs(html : str) -> list[str]:
+    return re.findall(r'src="(.*?)"', html) or []
+
+## CSS Extractors
+
+def css_import(css : str) -> list[str]:
+    return re.findall(r'@import\\s[\'"]?(.*?)[\'"]?;', css) or []
+
+def css_urls(css : str) -> list[str]:
+    return re.findall(r'url\([\'"]?(.*?)[\'"]?\)', css) or []
